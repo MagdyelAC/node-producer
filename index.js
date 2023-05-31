4ae5ec751ca20bb3f76c714e31b58f5f0bb2e826
MagdyelAC <99367866+MagdyelAC@users.noreply.github.com>
2023-05-31 11:04:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -22,24 +22,77 @@ app.get('/', (req, res, next) => {
 });
 
 const run = async (username) => {
+//ESTE ES EL PRIMERO
+const run = async (username) => {
+
+  await producer.connect()
+//    await producer.send()
+  await producer.send({
+    topic: 'test',
+    messages: [ 
+    { 
+      'value': `{"name": "${username}" }` 
+      } 
+        ],
+      })
+    await producer.disconnect()
+    }
+
+    app.get('/like', (req, res, next) => {
+    const username = req.query.name;
+    res.send({ 'name' : username } );
+    run(username).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+    });
+
+//ESTE ES EL DE REACCIONES
+const runReactions = async (userid, objectid, reactionid) => {
 
     await producer.connect()
 //    await producer.send()
     await producer.send({
-      topic: 'test',
-      messages: [ 
-	{ 
-	  'value': `{"name": "${username}" }` 
-  	} 
+      topic: 'reaction',
+      messages: [
+        {
+          'value': `{"userid": "${userid}","objectid": "${objectid}","reactionid": "${reactionid}" }` 
+        }
       ],
     })
-   await producer.disconnect()
+    await producer.disconnect()
 }
 
-app.get('/like', (req, res, next) => {
-  const username = req.query.name;
-  res.send({ 'name' : username } );
-  run(username).catch(e => console.error(`[example/producer] ${e.message}`, e))
+app.get('/reactions', (req, res, next) => {
+  const userid = req.query.userid;
+  const objectid = req.query.objectid;
+  const reactionid = req.query.reactionid;
+  res.send({ 'userid':userid, 'objectid': objectid, 'reactionid': reactionid });
+  runReactions(userid, objectid, reactionid).catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+});
+
+
+//ESTE ES EL DE COMENTARIOS
+const runComments = async (userid, objectid, message) => {
+
+  await producer.connect()
+//    await producer.send()
+  await producer.send({
+    topic: 'comment',
+    messages: [
+      {
+        'value': `{"userid": "${userid}","objectid": "${objectid}","message": "${message}" }` 
+      }
+    ],
+  })
+  await producer.disconnect()
+}
+
+app.get('/comments', (req, res, next) => {
+const userid = req.query.userid;
+const objectid = req.query.objectid;
+const message = req.query.message;
+res.send({ 'userid':userid, 'objectid': objectid, 'message': message });
+runComments(userid, objectid, message).catch(e => console.error(`[example/producer] ${e.message}`, e))
 
 });
 
